da52918fdf199560af22a0e7f9cf0cf1e734e146
kimmangchi <bbq5897@naver.com>
2024-12-18 10:48:43 +0900

    함수 선언 실습

--- Form1.cs
+++ Form1.cs
@@ -16,18 +16,19 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
 
-            byte classHumanNum = 30;
+            byte classHumanCount = 30;
             short seoulBusanDistance = 20000;
             int schoolTeacherAge = 28;
             float distanceToSun = 10000000000;
             double spaceRocketFire = 89.124;
             decimal koreaPeopleHairNum = 99999999999;
 
-            textBox_Print.Text += classHumanNum.GetType();
+            textBox_Print.Text += classHumanCount.GetType();
             textBox_Print.Text += " ";
-            textBox_Print.Text += "classHumanNum:";
+            textBox_Print.Text += "classHumanCount:";
             textBox_Print.Text += " ";
-            textBox_Print.Text += classHumanNum;
+            textBox_Print.Text += classHumanCount;
+            textBox_Print.Text += "num";
 
             textBox_Print.Text += "\r\n";
 
@@ -36,6 +37,7 @@ namespace WindowsFormsApp1
             textBox_Print.Text += "seoulBusanDistance:";
             textBox_Print.Text += " ";
             textBox_Print.Text += seoulBusanDistance;
+            textBox_Print.Text += "KM";
 
             textBox_Print.Text += "\r\n";
 
@@ -44,6 +46,7 @@ namespace WindowsFormsApp1
             textBox_Print.Text += "schoolTeacherAge:";
             textBox_Print.Text += " ";
             textBox_Print.Text += schoolTeacherAge;
+            textBox_Print.Text += "age";
 
             textBox_Print.Text += "\r\n";
 
@@ -52,6 +55,7 @@ namespace WindowsFormsApp1
             textBox_Print.Text += "distanceToSun:";
             textBox_Print.Text += " ";
             textBox_Print.Text += distanceToSun;
+            textBox_Print.Text += "KM";
 
             textBox_Print.Text += "\r\n";
 
@@ -60,6 +64,7 @@ namespace WindowsFormsApp1
             textBox_Print.Text += "spaceRocketFire:";
             textBox_Print.Text += " ";
             textBox_Print.Text += spaceRocketFire;
+            textBox_Print.Text += "Pa";
 
             textBox_Print.Text += "\r\n";
 
@@ -68,6 +73,31 @@ namespace WindowsFormsApp1
             textBox_Print.Text += "koreaPeopleHairNum:";
             textBox_Print.Text += " ";
             textBox_Print.Text += koreaPeopleHairNum;
+
+            int a = 100;
+            int b = 33;
+
+            int[] result = division(a, b);
+
+            textBox_Print.Text += "\r\n\r\n";
+
+            textBox_Print.Text += "quotient : ";
+            textBox_Print.Text += array[0];
+
+            textBox_Print.Text += "\r\n";
+
+            textBox_Print.Text += "remainder : ";
+            textBox_Print.Text += array[1];
         }
+
+        int[] division(int x, int y)
+        {
+            int[] result = new int[2];
+            result[0] = x / y;
+            result[1] = x % y;
+            
+            return result;
+        }
+
     }
 }
